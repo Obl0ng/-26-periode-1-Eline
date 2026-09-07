e83bcdb9666c3ca51ab64056686b5c64a2235c0d
Obl0ng <elinefrinks@gmail.com>
2026-09-07 12:21:45 +0200

    opdrachten
    oefenopdracth en weekopdracht

--- Oefenopdrachten/Opdracht 1/sketch.js
+++ Oefenopdrachten/Opdracht 1/sketch.js
@@ -1,7 +1,18 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
 }
 
 function draw() {
   background(220);
+  strokeWeight(4);
+  line(0, 20, 600, 20);
+  line(40, 0, 40, 400);
+  line(150, 0, 150, 200);
+  line(40, 200, 350, 200);
+  line(350, 200, 350, 400);
+  line(350, 250, 600, 250);
+  line(500, 20, 500, 250);
+  line(150, 100, 500, 100);
+  line(0, 325, 40, 325);
+  line(375, 20, 375, 100);
 }
--- Week 1 - Hello World/sketch.js
+++ Week 1 - Hello World/sketch.js
@@ -4,4 +4,12 @@ function setup() {
 
 function draw() {
   background(220);
+  text("1.", 25, 20);
+  text("Eline", 40, 20);
+  text("2.", 25, 40);
+  fill(255, 0, 0);  
+  rect(40, 30, 50, 10);
+  fill(225);
+  rect(40, 40, 50, 10);
+  rect(40, 50, 50, 10);
 }
