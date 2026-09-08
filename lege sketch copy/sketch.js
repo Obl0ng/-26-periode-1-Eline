cafbb0c75f4eab3eaa8435d37d6b60c95c739e0f
Obl0ng <elinefrinks@gmail.com>
2026-09-08 15:35:10 +0200

    week 1 opdracht

---
+++ lege sketch copy/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(220);
+  circle(200, 100, 80);
+  ellipse(180, 100, 25, 35);
+  ellipse(220, 100, 25, 35);
+  rect(180, 140, 40, 100);
+  line(180, 240, 150, 300);
+  line(220, 240, 245, 300);
+
+  
+  rect(400, 100, 120, 400);
+}
+
+
