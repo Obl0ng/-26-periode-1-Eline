65643a38e55be117b5b14c88de3a304b7e1b2493
Obl0ng <elinefrinks@gmail.com>
2026-09-10 14:13:04 +0200

    week 2

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -6,6 +6,9 @@ function draw() {
   background("lightblue");
   noStroke();
 
+  fill("yellow");
+  ellipse(200, 50, 70, 70);
+
   fill(200, 200, 200);
   ellipse(200, 90, 60, 60);
   ellipse(245, 85, 65, 65);
@@ -16,5 +19,12 @@ function draw() {
   ellipse(245, 95, 65, 65);
   ellipse(290, 100, 60, 60);
 
-  fill("200, 200, 200");
+  fill(200, 200, 200);
+  ellipse(240, 130, 60, 60);
+  ellipse(285, 125, 60, 60);
+  ellipse(330, 130, 60, 60);
+
+  fill("white");
+  ellipse(240, 140, 60, 60);
+  
 }
