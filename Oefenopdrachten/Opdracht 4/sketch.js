964d96705d53dee455e41982f6663f224d743340
Obl0ng <elinefrinks@gmail.com>
2026-09-15 15:44:21 +0200

    opdracht 4

--- Oefenopdrachten/Opdracht 4/sketch.js
+++ Oefenopdrachten/Opdracht 4/sketch.js
@@ -1,17 +1,25 @@
+
+
 function setup() {
   createCanvas(400, 400);
 }
 
-function keyPressed(){
- if (keyIsDown(66) === true) {  
+function draw() {
+  background(220);
+
+// Blokje
+if (keyIsPressed === true) { 
   if (keyCode === 66) {
-    }
+    fill("white");
+    rect(20, 20, 60, 60);
   }
 }
 
-function draw() {
-  background(220);
 fill("black");
 text("1.", 10, 15);
+text("2.", 10, 100);
+
+// Nummer
+
 
 }
