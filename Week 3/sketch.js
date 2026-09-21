558c2039d929f2c89110b60f6681fae7077963bc
Obl0ng <elinefrinks@gmail.com>
2026-09-21 14:32:04 +0200

    week 3

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -1,3 +1,22 @@
+let rectX1 = 105;
+let rectY1 = 105;
+let rectW = 60;
+let rectH = 60;
+
+let middenBoven = 0;
+
+function mousePressed(){
+	if (mouseButton == LEFT){
+
+    if(mouseX >= 170 && mouseX <= 230 && mouseY >= 105 && mouseY <= 165){
+        // We klikken in vakje midden boven
+        middenBoven = 1;
+    }
+
+
+	}
+}
+
 function setup() {
   createCanvas(400, 400);
 }
@@ -9,7 +28,21 @@ function draw() {
   rect(100, 100, 200, 200, 10);
   fill(200, 200, 200);
   rect(105, 105, 60, 60, 10);
+
+  if (middenBoven == 0){
+    fill("grey");
+  }
+  else if (middenBoven == 1){
+    fill("blue");
+  }
+  else if (middenBoven == 2){
+    fill("red");
+  }
+
   rect(170, 105, 60, 60, 10);
+
+
+  fill("grey");
   rect(235, 105, 60, 60, 10);
   rect(105, 170, 60, 60, 10);
   rect(170, 170, 60, 60, 10);
@@ -18,5 +51,7 @@ function draw() {
   rect(170, 235, 60, 60, 10);
   rect(235, 235, 60, 60, 10);
 
-  
+
+
+
 }
