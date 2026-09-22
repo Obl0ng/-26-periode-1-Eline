f39fe84958139abaeace48135ddc92ff68ff1aa9
Obl0ng <elinefrinks@gmail.com>
2026-09-22 12:43:49 +0200

    week 3

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -15,161 +15,195 @@ let linksOnder = 0;
 let middenOnder = 0;
 let rechtsOnder = 0;
 
-function mousePressed(){
-	if (mouseButton == LEFT){
-    turns = (turns + 1) % 2;
-    if(mouseX >= 105 && mouseX <= 165 && mouseY >= 105 && mouseY <= 165 && linksBoven == 0) {
-        linksBoven = turns + 1;
+function mousePressed() {
+  if (mouseButton == LEFT) {
+    // beurten + vakjes + mousepotion
+    if (mouseX >= 105 && mouseX <= 165 && mouseY >= 105 && mouseY <= 165 && linksBoven == 0) {
+      turns = (turns + 1) % 2;
+      linksBoven = turns + 1;
+      
     }
-    if(mouseX >= 170 && mouseX <= 230 && mouseY >= 105 && mouseY <= 165 && middenBoven == 0){
-        middenBoven = turns + 1;
+    if (mouseX >= 170 && mouseX <= 230 && mouseY >= 105 && mouseY <= 165 && middenBoven == 0) {
+      turns = (turns + 1) % 2;
+      middenBoven = turns + 1;
+      
     }
-    if(mouseX >= 235 && mouseX <= 295 && mouseY >= 105 && mouseY <= 165 && rechtsBoven == 0){
-        rechtsBoven = turns + 1;
+    if (mouseX >= 235 && mouseX <= 295 && mouseY >= 105 && mouseY <= 165 && rechtsBoven == 0) {
+      turns = (turns + 1) % 2;
+      rechtsBoven = turns + 1;
     }
-    if(mouseX >= 105 && mouseX <= 165 && mouseY >= 170 && mouseY <= 230 && linksMidden == 0){
-        linksMidden = turns + 1;
+    if (mouseX >= 105 && mouseX <= 165 && mouseY >= 170 && mouseY <= 230 && linksMidden == 0) {
+      turns = (turns + 1) % 2;
+      linksMidden = turns + 1;
     }
-    if(mouseX >= 170 && mouseX <= 230 && mouseY >= 170 && mouseY <= 230 && middenMidden == 0){
-        middenMidden = turns + 1;
+    if (mouseX >= 170 && mouseX <= 230 && mouseY >= 170 && mouseY <= 230 && middenMidden == 0) {
+      turns = (turns + 1) % 2;
+      middenMidden = turns + 1;
     }
-    if(mouseX >= 235 && mouseX <= 295 && mouseY >= 170 && mouseY <= 230 && rechtsMidden == 0){
-        rechtsMidden = turns + 1;
+    if (mouseX >= 235 && mouseX <= 295 && mouseY >= 170 && mouseY <= 230 && rechtsMidden == 0) {
+      turns = (turns + 1) % 2;
+      rechtsMidden = turns + 1;
     }
-    if(mouseX >= 105 && mouseX <= 165 && mouseY >= 235 && mouseY <= 295 && linksOnder == 0){
-        linksOnder =  turns + 1;
+    if (mouseX >= 105 && mouseX <= 165 && mouseY >= 235 && mouseY <= 295 && linksOnder == 0) {
+      turns = (turns + 1) % 2;
+      linksOnder = turns + 1;
     }
-    if(mouseX >= 170 && mouseX <= 230 && mouseY >= 235 && mouseY <= 295 && middenOnder == 0){
-        middenOnder = turns + 1;
+    if (mouseX >= 170 && mouseX <= 230 && mouseY >= 235 && mouseY <= 295 && middenOnder == 0) {
+      turns = (turns + 1) % 2;
+      middenOnder = turns + 1;
     }
-    if(mouseX >= 235 && mouseX <= 295 && mouseY >= 235 && mouseY <= 295 && rechtsOnder == 0){
-        rechtsOnder = turns + 1;
+    if (mouseX >= 235 && mouseX <= 295 && mouseY >= 235 && mouseY <= 295 && rechtsOnder == 0) {
+      turns = (turns + 1) % 2;
+      rechtsOnder = turns + 1;
     }
   }
 }
 
 function setup() {
   createCanvas(400, 400);
+  Crystal = loadFont("CrystalUniverse-Oblique.ttf")
 }
 
 function draw() {
-  background(220);
-  
+  // Achtergrond die veranderd
+  if (turns == 0) {
+    background("red");
+  }
+  else if (turns == 1) {
+    background("blue");
+  }
+
+  // Text
+  if (turns == 0) {
+    fill("black");
+    textSize(30);
+    textFont(Crystal);
+    text("red's turn", 75, 70);
+  }
+  if (turns == 1) {
+    fill("black");
+    textSize(30);
+    textFont(Crystal);
+    text("blue's turn", 75, 70);
+  }
+
+  // Zwarte rechthoek
   fill(0, 0, 0);
   rect(100, 100, 200, 200, 10);
-  
+
+
   // Linksboven
-  if (linksBoven == 0){
+  if (linksBoven == 0) {
     fill(200, 200, 200);
   }
-  else if (linksBoven == 1){
+  else if (linksBoven == 1) {
     fill("blue");
   }
-  else if (linksBoven == 2){
+  else if (linksBoven == 2) {
     fill("red");
   }
   rect(105, 105, 60, 60, 10);
 
 
   // Middenboven
-  if (middenBoven == 0){
+  if (middenBoven == 0) {
     fill(200, 200, 200);
   }
-  else if (middenBoven == 1){
+  else if (middenBoven == 1) {
     fill("blue");
   }
-  else if (middenBoven == 2){
+  else if (middenBoven == 2) {
     fill("red");
   }
   rect(170, 105, 60, 60, 10);
 
 
   // Rechtsboven
-  if (rechtsBoven == 0){
+  if (rechtsBoven == 0) {
     fill(200, 200, 200);
   }
-  else if (rechtsBoven == 1){
+  else if (rechtsBoven == 1) {
     fill("blue");
   }
-  else if (rechtsBoven == 2){
+  else if (rechtsBoven == 2) {
     fill("red");
   }
   rect(235, 105, 60, 60, 10);
 
 
   // Linksmidden
-  if (linksMidden == 0){
+  if (linksMidden == 0) {
     fill(200, 200, 200);
   }
-  else if (linksMidden == 1){
+  else if (linksMidden == 1) {
     fill("blue");
   }
-  else if (linksMidden == 2){
+  else if (linksMidden == 2) {
     fill("red");
   }
   rect(105, 170, 60, 60, 10);
 
 
   // Middenmidden
-  if (middenMidden == 0){
+  if (middenMidden == 0) {
     fill(200, 200, 200);
   }
-  else if (middenMidden == 1){
+  else if (middenMidden == 1) {
     fill("blue");
   }
-  else if (middenMidden == 2){
+  else if (middenMidden == 2) {
     fill("red");
   }
   rect(170, 170, 60, 60, 10);
 
 
   // Rechtsmidden
-  if (rechtsMidden == 0){
+  if (rechtsMidden == 0) {
     fill(200, 200, 200);
   }
-  else if (rechtsMidden == 1){
+  else if (rechtsMidden == 1) {
     fill("blue");
   }
-  else if (rechtsMidden == 2){
+  else if (rechtsMidden == 2) {
     fill("red");
   }
   rect(235, 170, 60, 60, 10);
 
 
   // Linksonder
-  if (linksOnder== 0){
+  if (linksOnder == 0) {
     fill(200, 200, 200);
   }
-  else if (linksOnder == 1){
+  else if (linksOnder == 1) {
     fill("blue");
   }
-  else if (linksOnder == 2){
+  else if (linksOnder == 2) {
     fill("red");
   }
   rect(105, 235, 60, 60, 10);
 
 
   // Middenonder
-  if (middenOnder == 0){
+  if (middenOnder == 0) {
     fill(200, 200, 200);
   }
-  else if (middenOnder == 1){
+  else if (middenOnder == 1) {
     fill("blue");
   }
-  else if (middenOnder == 2){
+  else if (middenOnder == 2) {
     fill("red");
   }
   rect(170, 235, 60, 60, 10);
 
 
   // Rechtsonder
-  if (rechtsOnder == 0){
+  if (rechtsOnder == 0) {
     fill(200, 200, 200);
   }
-  else if (rechtsOnder == 1){
+  else if (rechtsOnder == 1) {
     fill("blue");
   }
-  else if (rechtsOnder == 2){
+  else if (rechtsOnder == 2) {
     fill("red");
   }
   rect(235, 235, 60, 60, 10);
